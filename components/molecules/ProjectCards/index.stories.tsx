e0d883879623ccd8b8e9991e90c6651bc3379e26
Nacho <n.filipovskis@gmail.com>
2021-12-28 13:29:48 -0300

    fix: project stories

--- components/molecules/ProjectCards/index.stories.tsx
+++ components/molecules/ProjectCards/index.stories.tsx
@@ -11,7 +11,8 @@ const project: Project = {
     name: "Motorcyclist manager",
     description: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
     date: new Date(2019, 10, 2),
-    githubLink: "https://github.com/Nacho-Fili/motorcyclist-manager"
+    githubLink: "https://github.com/Nacho-Fili/motorcyclist-manager",
+    technologies: ["react", "node"],
 }
 
 export const Dark = () => <ProjectCards projects={[project, project, project]}/>
