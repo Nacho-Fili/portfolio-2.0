3e49860b194edd005d7c086f977c7cc9f4d8062e
Nacho <n.filipovskis@gmail.com>
2021-12-29 13:01:55 -0300

    fix: sidebar animation

--- components/organisms/Sidebar/index.tsx
+++ components/organisms/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Navbar from "../../molecules/Navbar"
 import Brand from "../../atoms/Brand"
 import styles from "./index.module.scss"
@@ -7,29 +7,41 @@ import { motion, Variants } from "framer-motion"
 import HamburguerButton from "../../atoms/HamburguerButton"
 
 const Sidebar: React.FC = () => {
-    const [collapsed, setCollapsed] = useState(true)
-
-    const sidebarWidth = 80
+    const [collapsed, setCollapsed] = useState<boolean>(true)
+    const [collapsedWidth, setCollapsedWidth] = useState<number>(0)
+    const [notCollapsedWidth, setNotCollapsedWidth] = useState<number>(0)
+    const [collapsedPosition, setCollapsedPosition] = useState<number | string>(0)
+    const [notCollapsedPosition, setNotCollapsedPosition] = useState<number | string>(0)
+    
+    useEffect(() => {
+        const isMobile = window.matchMedia("(max-width: 600px)").matches
+        setCollapsedWidth(isMobile ? 80 : 8)
+        setNotCollapsedWidth(isMobile ? 80 : 30)
+        setCollapsedPosition(isMobile ? -80 : "")
+        setNotCollapsedPosition(isMobile ? 0 : "")
+    }, [])
 
     return(
         <div style={{display: 'flex'}}>
             <HamburguerButton onClick={() => setCollapsed(prev => !prev)}/>
             <motion.div
-                initial={{ width: '8vw' }}
-                animate={{ width: collapsed ? '8vw': `${sidebarWidth}vw`}}
+                initial={{ 
+                    width: collapsed ? `${notCollapsedWidth}vw` : `${collapsedWidth}vw`,
+                    left: collapsed ? `${notCollapsedPosition}vw` : `${collapsedPosition}vw`,
+                }}
+                animate={{ 
+                    width: collapsed ? `${collapsedWidth}vw` : `${notCollapsedWidth}vw`,
+                    left: collapsed ? `${collapsedPosition}vw` : `${notCollapsedPosition}vw`,
+                }}
                 className={collapsed ? styles.collapsed : styles.notCollapsed}>
                 <Brand collapsed={collapsed} />
                 <Navbar collapsed={collapsed} />
                 <CollapseButton collapsed={collapsed} onClick={() => setCollapsed(prev => !prev)} />
             </motion.div>
-            {!collapsed && 
-                <motion.div 
-                    initial={{ background: '#000', opacity: 0 }}  
+            {!collapsed &&
+                <motion.div
+                    initial={{ background: '#000', opacity: 0 }}
                     animate={{ background: '#000', opacity: 0.8 }}
-                    style={{
-                        left: `${sidebarWidth}vw`,
-                        width: `${100-sidebarWidth}vw`
-                    }}
                     className={styles.cover} />}
         </div>
     )
