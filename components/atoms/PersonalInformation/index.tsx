489868fe64a271ce822c5182cf941d138e58c764
Nacho <n.filipovskis@gmail.com>
2022-01-11 11:41:26 -0300

    age fixed

--- components/atoms/PersonalInformation/index.tsx
+++ components/atoms/PersonalInformation/index.tsx
@@ -10,12 +10,21 @@ interface Props {
 const PersonalInformation: React.FC<Props> = ({ birthdate, name, lastname }) => {
     
     const parsedBirthdate = useMemo(() => new Date(birthdate), [birthdate])
+    const calculateAge = () => {
+        let age = +new Date().getFullYear() - parsedBirthdate.getFullYear();
+        if (+new Date().getMonth() - parsedBirthdate.getMonth() < 0) age--
+        else if (+new Date().getMonth() - parsedBirthdate.getMonth() === 0)
+            if(+new Date().getDate() - parsedBirthdate.getDate()) age--
+        
+        return age
+    }
+
 
     return(
         <div className={styles.container}>
             <p className={styles.text}>Fecha de nacimiento: {`0${parsedBirthdate.getDate()}-0${parsedBirthdate.getMonth()}-${parsedBirthdate.getFullYear()}`} </p>
             <p className={styles.text}>Nombre: {`${lastname.toUpperCase()}, ${name}`} </p>
-            <p className={styles.text}>Edad: {+new Date().getFullYear() - parsedBirthdate.getFullYear()} </p>
+            <p className={styles.text}>Edad: {calculateAge()} </p>
         </div>
     )
 }
